4ce8912950467b1d18d06c43712110021c48259f
Rauf <zen.star12@yahoo.com>
2021-02-14 01:39:17 +0700

    Tugas 17 Javascript Niomic

---
+++ tugas17.js
@@ -0,0 +1,13 @@
+function tugas17(){
+    var x = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+    var y = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+    console.log(x.toLowerCase());
+    console.log(x.toUpperCase());
+    console.log(y.toLowerCase());
+    console.log(y.toUpperCase());
+
+    var a = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    console.log(a.charCodeAt(0,),a.charCodeAt(1),a.charCodeAt(2),a.charCodeAt(3),a.charCodeAt(4),a.charCodeAt(5))
+    
+}
+tugas17()
